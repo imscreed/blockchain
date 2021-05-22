4a1967f57343c7acc19721ba045cbc59c9ee0aa2
Mohammed Ibrahim <mmohame4@lakeheadu.ca>
2021-05-22 18:25:15 -0400

    Implement Block

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * Importing the Block class
+ */
+//
+const BlockClass = require('./block/block.js');
+
+/**
+ * Creating a block object
+ */
+const block = new BlockClass.Block("Test Block");
+
+// Generating the block hash
+block.generateHash().then((result) => {
+	console.log(`Block Hash: ${result.hash}`);
+	console.log(`Block: ${JSON.stringify(result)}`);
+}).catch((error) => {console.log(error)});
